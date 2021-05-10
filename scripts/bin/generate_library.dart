c82220dd4a5f630c651a45f58b9c08a5599a7954
Alexander Thomas <athom@google.com>
2021-05-10 12:25:48 +0200

    Ensure that generate_library works offline

--- scripts/bin/generate_library.dart
+++ scripts/bin/generate_library.dart
@@ -7,16 +7,16 @@ import 'dart:io';
 
 import 'package:file/file.dart';
 import 'package:file/local.dart';
-import 'package:http/http.dart' as http;
 import 'package:scripts/src/library.dart';
 import 'package:scripts/src/versions.dart';
 
 /// Reads `versions.json` and generates the `dart` library definition.
 void main() {
-  generateLibrary(const LocalFileSystem(), http.read);
+  generateLibrary(const LocalFileSystem());
 }
 
-void generateLibrary(FileSystem fileSystem, HttpRead read) {
+void generateLibrary(FileSystem fileSystem) {
+  var read = (_, {headers}) => throw StateError("generate_library should work offline");
   var versions = versionsFromFile(fileSystem, read);
   stdout.write(buildLibrary(commit, versions));
 }
